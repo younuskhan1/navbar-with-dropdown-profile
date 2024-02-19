7b5be8e70b043dac1cd750a3c3b12070967116f9
younuskhan1 <md.younuskhan412@gmail.com>
2024-02-19 13:57:00 +0600

    code is understood here

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 let profileDropdownList = document.querySelector(".profile-dropdown-list");
+
+const toggle = () => profileDropdownList.classList.toggle("active");
+
+// The below task is additional but it is very important. Because sometimes the 
+// users want to close the dropdown menu by clicking anywhere of a website. 
+
 let btn = document.querySelector(".profile-dropdown-btn");
 
-let classList = profileDropdownList.classList;
-
-const toggle = () => classList.toggle("active");
-
-window.addEventListener("click", function (e) {
-  if (!btn.contains(e.target)) classList.remove("active");
+window.addEventListener("click", function (e){
+    // console.log(e.target);
+    // e.target is equal to where the user click(the clicked html element);
+  if (!btn.contains(e.target)){
+    profileDropdownList.classList.remove("active");
+  };
 });
